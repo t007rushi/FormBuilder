ad59107d401b7ba1e5a0bc0d97ca72d1ae6081e6
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-08-08 01:01:09 +0530

    style object file

---
+++ src/constants/formStyle.js
@@ -0,0 +1,14 @@
+export const formStyle = {
+    backgroundColor: "lightGray",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: "1rem",
+    width: "30rem",
+    position: "absolute",
+    left: "50%",
+    top: "50%",
+    transform: "translate(-50%, -50%)",
+    padding: "10px",
+  };
