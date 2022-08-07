18bea15b21630fb50e0dfc86cfa901a51be6749f
Rushikesh Tarapure <t007rushikesh@gmail.com>
2022-08-08 01:04:21 +0530

    textarea case added

--- src/utils/formRenderFunction.js
+++ src/utils/formRenderFunction.js
@@ -3,30 +3,37 @@ import { MultiSelect } from "../components/MultiSelect";
 import { Select } from "../components/Select";
 
 export const formRenderFunction = (Val, key) => {
-    switch (Val.field) {
-      case "input": {
-        return (
-          <div>
-            {key} : <Input {...Val} />
-          </div>
-        );
-      }
-      case "select": {
-        return (
-          <div>
-            {key} : <Select {...Val} />
-          </div>
-        );
-      }
-      case "multi-select": {
-        return (
-          <div>
-            {key} : <MultiSelect {...Val} />
-          </div>
-        );
-      }
-      default: {
-        return "";
-      }
+  switch (Val.field) {
+    case "input": {
+      return (
+        <div>
+          {key} : <Input {...Val} />
+        </div>
+      );
     }
-  };
+    case "select": {
+      return (
+        <div>
+          {key} : <Select {...Val} />
+        </div>
+      );
+    }
+    case "multi-select": {
+      return (
+        <div>
+          {key} : <MultiSelect {...Val} />
+        </div>
+      );
+    }
+    case "textarea": {
+      return (
+        <div>
+          {key} : <textarea />
+        </div>
+      );
+    }
+    default: {
+      return "";
+    }
+  }
+};
